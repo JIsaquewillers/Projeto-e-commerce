130c30ca629c13bc7200be961adbe79611615e17
JIsaquewillers <joaoisaquewillers@gmail.com>
2026-03-20 12:19:29 -0300

    .

---
+++ script.js
@@ -0,0 +1,21 @@
+//variáveis
+let nome = "Sandro"
+
+//array / coleção de valores
+let colecaoAluns = new Array('isaque','maman','thomas');
+let colecaoAlunos2 = ['brayan','samuel','enrico'];
+
+
+//pergunta para o usuário um número
+let posicao = prompt('digite um nnúmero de 0 a 3');
+
+//mostra na tela
+console.log(colecaoAlunos[posicao]);
+
+//
+console.log(colecaoAlunos2);
+
+////////////////////////////
+
+let produtos = new Array('suco','computador','bife','ps5','chocolate','lapis','caderno','fogão','martelo','peixe')
+console.log(produtos)
