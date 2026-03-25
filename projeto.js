bb2f4331b4ee4df5adb1f47730e7cec3c5b6226a
JIsaquewillers <joaoisaquewillers@gmail.com>
2026-03-25 09:30:46 -0300

    continuação

---
+++ projeto.js
@@ -0,0 +1,17 @@
+const carrinhoBTN = document.getElementById("carrinhobtn")
+const buttonADD = document.getElementsByClass("botãoadd")
+
+let produtos = new Array('ovo1','ovo2','ovo3','ovo4','ovo5','ovo6')
+
+
+//////
+buttonADD.addEventListener("click",()=>{
+
+})
+
+
+
+//////
+carrinhoBTN.addEventListener('click',() =>{
+
+})
