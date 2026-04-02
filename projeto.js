5ff578fea75043f0ef68558096037452015e2b31
JIsaquewillers <joaoisaquewillers@gmail.com>
2026-04-02 09:07:21 -0300

    continuação js
    terminei o botão de abrir e fechar o carrinho.

--- projeto.js
+++ projeto.js
@@ -1,34 +1,47 @@
-//*abrir carrinho na lateral da página
+//*abrir carrinho na lateral da página X
 //*adiconar item no carrinho
 //*aparecer o número de produtos no carrinho
 //*somar preço
 
-
+//declaração de variáveis e array //
 
 const carrinhoBTN = document.getElementById("carrinhobtn")
-const buttonADD = document.getElementsByClass("botãoadd")
+const fecharCart = document.getElementById('fecharCart')
+const buttonADD = document.querySelectorAll("botãoadd")
 const cart = document.getElementById("cart")
 let produtos = new Array()
 
 
-// evneto de clique no botão de adicionar item, para adiconar o item no carrinho. //
-buttonADD.addEventListener("click",()=>{
-        const nome = document.getElementById("nome").textContent
-        const preço = document.getElementById("preço").textContent
-
-    produtos.push(
-        nome,preço
-    )
-})
-
 
 // evento de clique no botão do carrinho, para abir o carrinho na lateral da página. // 
 carrinhoBTN.addEventListener('click',() =>{
+    
     if( cart.style.display ==='none'){
-        cart.style.display === 'block'
+        cart.style.display = 'block'
     }else{
-        cart.style.display === 'none'
-    }}) 
+        cart.style.display = 'none'
+    }
+}) 
+
+//evento de clique no botão de fechar o carrinho //
+fecharCart.addEventListener('click', () =>{
+    if(cart.style.display === 'block'){
+        cart.style.display = 'none'
+    }else{
+        cart.style.display = 'block'
+    }
+})
+   
+// evneto de clique no botão de adicionar item, para adiconar o item no carrinho. 
+// carrinhoBTN.array.forEach(button => { button.addEventListener('click',()=> ) } ); //
+
+    
+
+
+
+
+
+
 
 
     
